44090eaad6d5bc716278113ca35ae2e6d6f2c108
Luis Antonio <luiscoronatapon23@gmail.com>
2025-11-12 23:38:36 -0600

    FIX: Eliminadas credenciales por defecto de db.js por seguridad

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "luis1008",
-  database: process.env.DB_NAME || "zapateria_eleganzza_shoes",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10) || 10,
   queueLimit: 0,
